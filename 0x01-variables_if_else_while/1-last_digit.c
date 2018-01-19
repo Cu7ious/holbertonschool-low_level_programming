dcab4f47bdf5e79b598ccfac0e93ce4535498cc6
Sergii Garkusha <328@holbertonschool.com>
2018-01-18 17:41:54 -0800

    Replaced n with lastNum

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,12 +17,12 @@ int main(void)
 
 	lastNum = n % 10;
 
-	if (n == 0)
-		printf("Last digit of %d is %d and is 0\n", n, lastNum);
-	else if (n > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastNum);
+	if (lastNum == 0)
+		printf("Last digit of %i is %i and is 0\n", n, lastNum);
+	else if (lastNum > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, lastNum);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastNum);
 
 	return (0);
 }
