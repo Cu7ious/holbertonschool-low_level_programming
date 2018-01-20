4132c0d2092eadcc8b686ae76106821878f1cefa
Sergii Garkusha <328@holbertonschool.com>
2018-01-19 19:43:53 -0800

    Added newline char

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,28 +12,26 @@ int main(void)
 	int o;
 
 	for (h = 0; h <= 9; h++)
-	{
 		for (t = 0; t <= 9; t++)
-		{
 			if (t > h)
-			{
 				for (o = 0; o <= 9; o++)
-				{
 					if (o > t)
 					{
-						if (!(t == 0 && t == 1 && o == 2))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-						putchar(h + '0');
-						putchar(t + '0');
-						putchar(o + '0');
+					if (!(
+						h == 0
+						&& t == 1
+						&& o == 2
+						))
+					{
+						putchar(',');
+						putchar(' ');
 					}
-				}
-			}
-		}
-	}
+					putchar(h + '0');
+					putchar(t + '0');
+					putchar(o + '0');
+					}
+
+	putchar('\n');
 
 	return (0);
 }
