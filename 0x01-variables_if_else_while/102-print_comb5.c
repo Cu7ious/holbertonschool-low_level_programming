4684fd65ca66605dfecc02ecfe6c76b59913d967
Sergii Garkusha <328@holbertonschool.com>
2018-01-20 00:05:21 -0800

    Test advanced solution

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - root function
+ *
+ * Return: always 0
+ **/
+int main(void)
+{
+	int t;
+	int s;
+	int d;
+	int e;
+
+	for (t=0;t<=9;t++)
+	{
+		for (s=0;s<=9;s++)
+		{
+			for (d=0;d<=9;d++)
+			{
+				for (e=0;e<=9;e++)
+				{
+					if ((t<d && s<=e) || (t==d && s<e))
+					{
+						putchar(t +'0');
+						putchar(s +'0');
+						putchar(' ');
+						putchar(d +'0');
+						putchar(e +'0');
+
+						if (!(t==9 && s==8))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return (0);
+}
