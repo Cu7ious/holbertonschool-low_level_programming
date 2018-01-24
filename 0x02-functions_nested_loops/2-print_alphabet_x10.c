108f1220c983951901e46e3a48adce05e09eca48
Sergii Garkusha <328@holbertonschool.com>
2018-01-23 16:53:33 -0800

    Added function that prints a-z 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ * 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int ten = 10;
+
+	while(ten)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+
+		ten--;
+	}
+}
