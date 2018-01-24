179cef2f97cc911251fc1e9174452c5787b5f9d0
Sergii Garkusha <328@holbertonschool.com>
2018-01-23 19:48:05 -0800

    Added header, code & doc for the task #7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the sign of a number
+ *
+ * @n: int number
+ * Return: last digit of a number
+ */
+int print_last_digit(int n)
+{
+	int result;
+
+	if (n > 0)
+		result = n % 10;
+	else if (n < 0)
+		result = (-1 * n) % 10;
+	else
+		result = 0;
+
+	_putchar(result + '0');
+	return (result);
+}
