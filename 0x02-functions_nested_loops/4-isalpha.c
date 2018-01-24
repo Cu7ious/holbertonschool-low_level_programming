51956a6f2177c91eb19226dc50454615eefcdcd4
Sergii Garkusha <328@holbertonschool.com>
2018-01-23 17:49:06 -0800

    Added header, docs and code for task #4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isalpha- checks for alphabetical chars
+ *
+ * @c: ASCII character
+ * Return: 1 if [A-Za-z], 0 if not
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'Z' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+
+	return (0);
+}
