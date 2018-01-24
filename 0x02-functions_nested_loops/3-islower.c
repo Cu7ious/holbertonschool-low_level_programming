f5450cf171f33a8cf9b8de4159f2d732c3530dae
Sergii Garkusha <328@holbertonschool.com>
2018-01-23 17:40:57 -0800

    Fix styles

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,13 @@
 /**
  * _islower - checks for lowercase character
  *
+ * @c: ASCII character
  * Return: 1 if lowercase, 0 if not lowercase
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return 1;
+		return (1);
 
-	return 0;
+	return (0);
 }
