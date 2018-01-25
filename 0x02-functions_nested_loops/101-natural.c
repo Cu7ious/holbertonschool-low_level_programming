46f92990600edaf15a1cff48c328d8fc0b28ea8a
Sergii Garkusha <328@holbertonschool.com>
2018-01-24 20:36:49 -0800

    Decoupled it to a function

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _printer -  computes and prints the sum of all the multiples
+ * of 3 or 5 below the certain (excluded)
+ * @n: the number
+ * Return: nothing
  */
-int main(void)
+void _printer(int n)
 {
 	int i;
 	int t = 0;
 	int f = 0;
 
-	for (i = 0; i < 1024; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (i % 3 == 0)
 			t += i;
@@ -22,6 +23,15 @@ int main(void)
 	}
 
 	printf("%d\n", t + f);
+}
 
-    return (0);
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_printer(1024);
+	return (0);
 }
