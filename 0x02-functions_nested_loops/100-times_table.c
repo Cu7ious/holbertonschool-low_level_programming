f4c958fbc4007b0ca57ea50308df0051cd405cbf
Sergii Garkusha <328@holbertonschool.com>
2018-01-24 20:01:03 -0800

    Fixed styles

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,40 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- *print_times_table - prints the n times table, starting with 0
+ * _printer - prints spaces & result (private)
+ *
+ * @p: int number of spaces
+ * @s: int result of multiplication
+ *
+ * Return: nothing
+ */
+void _printer(int p, int s)
+{
+	while (p)
+	{
+		_putchar(' ');
+		p--;
+	}
+
+	if (s >= 100)
+	{
+		_putchar(s / 100 + '0');
+		_putchar(s % 100 / 10 + '0');
+		_putchar(s % 10 + '0');
+	}
+	else if (s >= 10)
+	{
+		_putchar(s / 10 + '0');
+		_putchar(s % 10 + '0');
+	}
+	else
+	{
+		_putchar(s + '0');
+	}
+}
+
+/**
+ * print_times_table - prints the n times table, starting with 0
  *
  * @n: int number
  *
@@ -12,7 +45,6 @@ void print_times_table(int n)
 	int i;
 	int ii;
 	int result;
-	char comma;
 
 	if (n < 0 || n > 15)
 		return;
@@ -22,17 +54,32 @@ void print_times_table(int n)
 		for (ii = 0; ii <= n; ii++)
 		{
 			result = i * ii;
-			comma = (ii != n) ? ',' : '\0';
 
 			if (result >= 100)
-				printf(" %d%c", result, comma);
+			{
+				_printer(1, result);
+				if (ii != n)
+					_putchar(',');
+			}
 			else if (result >= 10)
-				printf("  %d%c", result, comma);
+			{
+				_printer(2, result);
+				if (ii != n)
+					_putchar(',');
+			}
 			else if (ii == 0)
-				printf("%d%c", result, comma);
+			{
+				_printer(0, result);
+				if (ii != n)
+					_putchar(',');
+			}
 			else
-				printf("   %d%c", result, comma);
+			{
+				_printer(3, result);
+				if (ii != n)
+					_putchar(',');
+			}
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
