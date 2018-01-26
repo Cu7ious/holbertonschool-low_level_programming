89cecb865450152ae933d373ef73b0484db43198
Sergii Garkusha <328@holbertonschool.com>
2018-01-25 22:04:28 -0800

    Added code & header for task #2

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: multiplayer
+ * @b: multiplayee
+ *
+ * Return: 1 if digit, else 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
