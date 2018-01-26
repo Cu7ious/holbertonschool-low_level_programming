8c8da4454b06cdd02c1e31f5b06c68a407d0a224
Sergii Garkusha <328@holbertonschool.com>
2018-01-26 01:42:09 -0800

    Fixed the solution

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
-			printf("Buzz ");
+			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
