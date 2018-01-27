94d3ea3ecbff76677b78eaef7f6526c96d305a6f
Sergii Garkusha <328@holbertonschool.com>
2018-01-26 16:59:57 -0800

    Refined solution

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,27 @@
 #include "holberton.h"
 
 /**
+ * printer - prints the line of numbers
+ * followed by a new line
+ * @n: the length of a line
+ *
+ * Return: void
+ */
+void printer(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i > 9)
+			_putchar(i / 10 + '0');
+
+		_putchar(i % 10 + '0');
+	}
+	_putchar('\n');
+}
+
+/**
  * more_numbers - 10 times the numbers, from 0 to 14
  * followed by a new line
  *
@@ -8,22 +29,11 @@
  */
 void more_numbers(void)
 {
-	int ten_times;
-	int fourteen;
-	int first_char;
+	int i = 0;
 
-	for (ten_times = 0; ten_times <= 10; ten_times++)
+	while (i < 10)
 	{
-		for (fourteen = 0; fourteen <= 14; fourteen++)
-		{
-			first_char = fourteen < 10 ? fourteen : fourteen / 10;
-
-			_putchar(first_char + '0');
-
-			if (fourteen > 9)
-				_putchar(fourteen % 10 + '0');
-
-		}
-		_putchar('\n');
+		printer(14);
+		i++;
 	}
 }
