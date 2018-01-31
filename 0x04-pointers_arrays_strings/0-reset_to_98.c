9482efd9cdf8178b9ca98f8a74b869e2bdf9276d
Sergii Garkusha <328@holbertonschool.com>
2018-01-30 16:13:46 -0800

    Added function proto with empty body

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,11 @@
 /**
  * reset_to_98 - takes a pointer to an int as param
  * and updates the value it points to to 98
+ * @n: pointer
  *
  * Return: nothing
  */
 void reset_to_98(int *n)
 {
-	
+
 }
