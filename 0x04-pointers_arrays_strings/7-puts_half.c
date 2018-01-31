56048832acff166651845f1e37c3a95d43cac0e1
Sergii Garkusha <328@holbertonschool.com>
2018-01-30 20:47:01 -0800

    Added function signature, body & docs for task #7

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of a string,
+ * followed by a new line
+ * @str: string of characters
+ *
+ * Return: nothing.
+ */
+void puts_half(char *str)
+{
+
+}
