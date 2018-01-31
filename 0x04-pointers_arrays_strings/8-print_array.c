a293796401f485e92219594cdf30780ebe5d9d16
Sergii Garkusha <328@holbertonschool.com>
2018-01-30 18:40:50 -0800

    Added func signature & docs

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_array - prints n elements of an array
+ * of integers, followed by a new line
+ * @a: array of integers
+ * @n: lengts of the array
+ *
+ * Return: nothing.
+ */
+void print_array(int *a, int n)
+{
+
+}
