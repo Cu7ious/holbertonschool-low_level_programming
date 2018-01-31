ac851481e1684ad7032580a663100c941cd728e8
Sergii Garkusha <328@holbertonschool.com>
2018-01-30 21:11:32 -0800

    Added solution for task #7

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * puts_half - prints half of a string,
@@ -9,5 +10,19 @@
  */
 void puts_half(char *str)
 {
+	int length = 0;
+	int odd_even;
+	int i;
 
+	while (str[length] != 0)
+		length++;
+
+	odd_even = length % 2 == 0
+		? length / 2
+		: (length - 1) / 2;
+
+	for (i = 0; i < odd_even; i++)
+		_putchar(str[odd_even + i]);
+
+	_putchar('\n');
 }
