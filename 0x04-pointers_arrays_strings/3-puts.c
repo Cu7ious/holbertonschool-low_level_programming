7c0e7a05977a8071692a3a55b2562031eda87be4
Sergii Garkusha <328@holbertonschool.com>
2018-01-30 16:45:58 -0800

    Added func proto & doc

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: array of characters
+ *
+ * Return: nothing.
+ */
+void _puts(char *str)
+{
+}
