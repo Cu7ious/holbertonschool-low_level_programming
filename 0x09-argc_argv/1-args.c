2d8d738977641da425a34321747de6e3b5a9da08
Sergii Garkusha <328@holbertonschool.com>
2018-02-11 21:27:16 -0800

    Get rid of argv

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
+ * main - prints the number of arguments passed into it
+ * @argc: quantity of CLI arguments (args count)
+ * @argv: array of keys (args vector)
  *
- *
- *
- *
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	if (argv[1])
-		printf("%d\n", argc - 1);
-	else
-		printf("%d\n", 0);
+	(void) *argv;
+
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
