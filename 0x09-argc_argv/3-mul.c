3da2acc8ea7772d2390865cd0f9da93c98ce4b27
Sergii Garkusha <328@holbertonschool.com>
2018-02-11 21:44:53 -0800

    Added function, body doc and solution for task #3

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: quantity of CLI arguments (args count)
+ * @argv: array of keys (args vector)
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 2)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+
+	printf("Error\n");
+	return (1);
+}
