7b90668d20852500f34128ea4b0e812c6d024c2a
Sergii Garkusha <328@holbertonschool.com>
2018-02-15 23:29:41 -0800

    Added solution for task #4

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * free_grid - free memory of 2-d array
+ * @grid: 2-d array
+ * @height: the height of 2-d array
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height == 0)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
