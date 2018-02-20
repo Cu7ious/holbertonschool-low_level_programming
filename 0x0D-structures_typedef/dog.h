b1ade26c51d936bd954f5382bd72c427982bc3f0
Sergii Garkusha <328@holbertonschool.com>
2018-02-19 20:19:37 -0800

    Get rid of typedef, commented all protos

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -9,17 +9,13 @@
  *
  * Description: We are building a dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
 
 int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif /* _DOG_H_ */
