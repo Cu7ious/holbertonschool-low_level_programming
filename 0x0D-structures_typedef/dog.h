eb2759ac0e7b0cefd7d64debfa4c40cd249f041e
Sergii Garkusha <328@holbertonschool.com>
2018-02-19 20:13:22 -0800

    Added struct dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,26 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct Dog - a dog data structure
+ * @name: Dog's name, string
+ * @age: Dog's age, float
+ * @owner: Dog Owner's name, string
+ *
+ * Description: We are building a dog
+ */
+struct Dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog - Typedef for struct Dog
+ */
+typedef struct Dog dog;
+
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
