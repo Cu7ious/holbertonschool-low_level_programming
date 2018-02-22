082b4286f46da9ff3559c1897562de1b1e8350a5
Sergii Garkusha <328@holbertonschool.com>
2018-02-21 19:26:30 -0800

    Added header with protos && functions for task #3

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+/**
+ * op_add - adds a to b
+ * @a: 1st operand
+ * @b: 2nd operand
+ *
+ * Return: result, int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - subtracts a - b
+ * @a: 1st operand
+ * @b: 2nd operand
+ *
+ * Return: result, int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - multiplies a * b
+ * @a: 1st operand
+ * @b: 2nd operand
+ *
+ * Return: result, int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - divides a / b
+ * @a: 1st operand
+ * @b: 2nd operand
+ *
+ * Return: result, int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - finds mofulus of a % b
+ * @a: 1st operand
+ * @b: 2nd operand
+ *
+ * Return: result, int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
