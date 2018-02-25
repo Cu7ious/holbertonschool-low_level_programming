e6a75cf360e5b507e72be967dbc31e9a47c2f60e
Sergii Garkusha <328@holbertonschool.com>
2018-02-24 18:53:27 -0800

    Added solution for task #0

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: variable unmber of args, int
+ *
+ * Return: sum of all params, int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	unsigned int sum = 0;
+
+	if (n == sum)
+		return (sum);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
+}
