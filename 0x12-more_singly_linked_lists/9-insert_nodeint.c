8f5d5ab35c51976e1db608252f794ade6c6f52e3
Sergii Garkusha <328@holbertonschool.com>
2018-03-06 23:18:47 -0800

    Added solution for the task #9

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,5 +10,34 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *tempHead;
+	listint_t *prevNode;
+	listint_t *node;
+	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
+
+	tempHead = *head;
+	for (i = 0; i <= idx; i++)
+	{
+		if (i + 1 == idx)
+			prevNode = tempHead;
+		else if (i == idx)
+		{
+			node = malloc(sizeof(listint_t));
+			if (node == NULL)
+				return (NULL);
+
+			node->n = n;
+			node->next = tempHead;
+			prevNode->next = node;
+
+			return (node);
+		}
+
+		tempHead = tempHead->next;
+	}
+
+	return (NULL);
 }
