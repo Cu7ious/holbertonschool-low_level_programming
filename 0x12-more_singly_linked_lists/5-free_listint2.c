d89b723168f691cecae3a4fc03d4e355689f5ce9
Sergii Garkusha <328@holbertonschool.com>
2018-03-07 09:58:47 -0800

    Simplified solution

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -8,19 +8,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *a, *b;
+	listint_t *temp;
 
 	if (head == NULL || *head == NULL)
 		return;
 
-	a = *head;
-
-	while (a != NULL)
+	while (*head != NULL)
 	{
-		b = a->next;
-		free(a);
-		a = b;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
-
-	*head = NULL;
 }
