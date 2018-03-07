d314aa0fecd79966ab2271df04c1eea17b24ba63
Sergii Garkusha <328@holbertonschool.com>
2018-03-07 15:31:30 -0800

    Fixed the solution

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = (*head)->next;
 		free(pointer);
-		return (0);
+		return (1);
 	}
 
 	while (pointer != NULL && i <= index)
