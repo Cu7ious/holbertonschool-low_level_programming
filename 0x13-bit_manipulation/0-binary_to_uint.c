1fd2b3ad47bcd5586172f87dfb419dd6ea547450
Sergii Garkusha <328@holbertonschool.com>
2018-03-08 21:26:44 -0800

    Added doc & declaration

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string that represents binary number
+ *
+ * Return: unsigned int, converted from binary
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	if (!b)
+		return (NULL);
+}
