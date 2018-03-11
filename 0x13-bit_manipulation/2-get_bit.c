56b87daaef51e1989f85b4f3fcbfd9df1a80d253
Sergii Garkusha <328@holbertonschool.com>
2018-03-10 18:32:11 -0800

    Simplified solution for the task #2

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -9,16 +9,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int b;
-
 	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 
-	if (((n >> index) & 1) == 0 || ((n >> index) & 1) == 1)
-	{
-		b = n >> index & 1;
-		return (b);
-	}
-
-	return (-1);
+	return (n >> index & 1);
 }
