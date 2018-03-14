865a4046b978c2dac88f4c791854d713f76bf395
Sergii Garkusha <328@holbertonschool.com>
2018-03-13 22:47:53 -0700

    Changed check to NULL

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -28,21 +28,23 @@ int _strlen(char *s)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	ssize_t fd, w, len = _strlen(text_content);
+	ssize_t fd, w, len;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
 	if (fd == -1)
 		return (-1);
-	if (len == 0)
+	if (text_content == NULL)
 		return (1);
 
+	len = _strlen(text_content);
 	w = write(fd, text_content, len);
 	if (w == -1)
 		return (-1);
 
 	close(fd);
+
 	return (1);
 }
