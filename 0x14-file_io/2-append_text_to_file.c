8cd2a127130f43bfa4a8cb75bd8f619465565a97
Sergii Garkusha <328@holbertonschool.com>
2018-03-13 22:01:56 -0700

    Added doc and declaration for task#2

---
+++ 0x14-file_io/2-append_text_to_file.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - ppends a text at the end of a file
+ * @filename: the name of the file
+ * @text_content: text to add
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+}
