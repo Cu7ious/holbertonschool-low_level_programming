febd5cf9042a46f01a7e7a2b794d5d7b1330db44
Sergii Garkusha <328@holbertonschool.com>
2018-03-13 23:06:48 -0700

    Added truncation mask

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -33,11 +33,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+
 	if (text_content == NULL)
-		return (1);
+		text_content = "";
 
 	len = _strlen(text_content);
 	w = write(fd, text_content, len);
