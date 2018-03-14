1d3672d42b31f54d9718ec52a01ca8ead6bfba54
Sergii Garkusha <328@holbertonschool.com>
2018-03-13 21:40:14 -0700

    Added check for case when letters == 0

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -14,6 +14,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
+	if (letters == 0)
+		return (0);
 
 	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
