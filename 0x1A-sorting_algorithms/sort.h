092ab3c5f6ee9c61beaeac24b7741cc53882ed75
Cu7ious <328@holbertonschool.com>
2018-05-29 22:48:24 +0000

    Added proto of the bubble_sort() function

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -1,5 +1,6 @@
 #ifndef __SORT_H__
 #define __SORT_H__
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -10,12 +11,15 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/* task #0 */
+void bubble_sort(int *array, size_t size);
 
 #endif
