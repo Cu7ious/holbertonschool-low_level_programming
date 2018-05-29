880e3ce8d9ab5424ce48181e7f16c5a0bbb3e14c
Cu7ious <328@holbertonschool.com>
2018-05-29 23:33:55 +0000

    Added the essentials for the task #1

---
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - implementation of the insertion sort
+ * @list: the list to sort
+ *
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+}
--- 0x1A-sorting_algorithms/README.md
+++ 0x1A-sorting_algorithms/README.md
@@ -3,5 +3,6 @@
 ##### Summary
 
 ```
-
+0-bubble_sort.c, 0-O		// Bubble sort algorithm
+1-insertion_sort_list.c, 1-O	// Insertion sort algorithm
 ```
