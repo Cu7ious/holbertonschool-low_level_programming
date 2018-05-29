d7f02634ce036b4f7328ae6c86ae3f70d83ddca1
Cu7ious <328@holbertonschool.com>
2018-05-29 22:49:04 +0000

    Implemented the bubble sort algorithm

---
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array with the bubble sort algorithm
+ * @array: an array to sort
+ * @size: the size of array
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int a, b;
+	size_t i = 0;
+	size_t j = size;
+
+	if (size < 1)
+		return;
+
+	while (j)
+	{
+		for (i = 0; i < j; i++)
+			if (i + 1 < j)
+			{
+				a = array[i];
+				b = array[i + 1];
+
+				if (a > b)
+				{
+					array[i + 1] = a;
+					array[i] = b;
+				}
+			}
+
+		--j;
+	}
+}
