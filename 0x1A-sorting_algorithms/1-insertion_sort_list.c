e964156bc3bdb897dde93be408334d13154051a2
Cu7ious <328@holbertonschool.com>
2018-05-30 02:12:37 +0000

    Added the insertion sort

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,5 +1,18 @@
 #include "sort.h"
 
+void swap(listint_t *prev, listint_t *curr)
+{
+	curr->prev = prev->prev;
+	prev->next = curr->next;
+	prev->prev = curr;
+	curr->next = prev;
+
+	if (curr->prev)
+		curr->prev->next = curr;
+	if (prev->next)
+		prev->next->prev = prev;
+}
+
 /**
  * insertion_sort_list - implementation of the insertion sort
  * @list: the list to sort
@@ -8,4 +21,38 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *curr, *first;
+	int i, c = 0;
+
+	if (list == NULL || (*list) == NULL)
+		return;
+
+	first = *list;
+	curr = *list;
+
+	while (curr != NULL)
+	{
+		c++;
+		while (curr->prev != NULL)
+			if (curr->n < curr->prev->n)
+			{
+				swap(curr->prev, curr);
+
+				if (curr->prev == NULL)
+				{
+					first = curr;
+					*list = first;
+				}
+
+				print_list(*list);
+			}
+			else
+				break;
+
+		for (i = 0; i < c; i++)
+			if (i == 0)
+				curr = first->next;
+			else
+				curr = curr->next;
+	}
 }
