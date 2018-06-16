9792fc32328fcd12567aa191aac5e50560fa8df1
Cu7ious <328@holbertonschool.com>
2018-06-16 05:11:44 +0000

    Created 0 represents a water zone

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module that contains island_perimeter function
+"""
+
+
+def island_perimeter(grid):
+    """ Returns the perimeter of the island described in grid
+
+        Args:
+            grid: list of list of integers (matrix)
+                0 represents a water zone
+                1 represents a land zone
+                One cell is a square with side length 1
+                Grid cells are connected hor/vert (not diagonally)
+                Grid is rectangular, width and height don’t exceed 100
+                Grid is surrounded by water, and there is one island (or zero)
+                The island doesn’t have 'lakes' (water inside that isn't
+                    connected to the water around the island)
+    """
+    pass
