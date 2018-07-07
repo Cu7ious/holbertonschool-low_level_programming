3863bcf986afada7a51f19c61401d5902b9aa367
Cu7ious <328@holbertonschool.com>
2018-07-07 05:29:18 +0000

    Added solution for the task #3

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -1,7 +1,7 @@
 #ifndef __BINARY_TREES_H__
 #define __BINARY_TREES_H__
 
-#include <stdlib.io>
+#include <stdlib.h>
 #include <stddef.h>
 
 /**
@@ -25,5 +25,6 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif /* __BINARY_TREES_H__ */
